48a695669b67adf9b1e30a5ccae81c32cd9fb8cd
Herbert Greenlee <greenlee@fnal.gov>
2018-08-15 13:11:52 -0500

    Merge remote-tracking branch 'origin/feature/mstancar_crt_tracking' into v06_26_01_br

--- ubobj/CRT/classes.h
+++ ubobj/CRT/classes.h
@@ -12,6 +12,8 @@
 #include "ubobj/CRT/CRTSimData.hh"
 #include "ubobj/CRT/CRTHit.hh"
 #include "ubobj/CRT/CRTTrack.hh"
+#include "ubobj/CRT/CRTTzero.hh"
+#include "canvas/Persistency/Common/Assns.h"
 ////#include "MSetCRTFrag.hh"
 #include <utility>
 #include <vector>
@@ -32,8 +34,18 @@ template class std::map< unsigned char, std::vector< std::pair<int,float> > >;
 template class std::vector<crt::CRTHit>;
 template class art::Wrapper< std::vector<crt::CRTHit> >;
 
+template class std::vector<crt::CRTTzero>;
+template class art::Wrapper< std::vector<crt::CRTTzero> >;
+
 template class std::vector<crt::CRTTrack>;
 template class art::Wrapper< std::vector<crt::CRTTrack> >;
+
+//template class std::pair< art::Ptr<crt::CRTTzero>,        art::Ptr<crt::CRTHit>    >;
+//template class std::pair< art::Ptr<crt::CRTHit>,        art::Ptr<crt::CRTTzero>    >;
+template class art::Assns<crt::CRTTzero,crt::CRTHit, void>;
+template class art::Assns<crt::CRTHit,crt::CRTTzero, void>;
+template class art::Wrapper<art::Assns<crt::CRTTzero,crt::CRTHit, void> >;
+template class art::Wrapper<art::Assns<crt::CRTHit,crt::CRTTzero, void> >;
 
 ////template class std::vector<crt::MSetCRTFrag>;
 ////template class art::Wrapper< std::vector<crt::MSetCRTFrag> >;
