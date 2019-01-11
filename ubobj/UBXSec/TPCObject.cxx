a7d22e9a186b3367e2db9f326e9f9678863f6c44
kduffy <kduffy@fnal.gov>
2019-01-11 16:13:10 -0600

    Add functions to convert TPCObject origin enums to human-readable strings because I can't be bothered looking up what it means every time

--- ubobj/UBXSec/TPCObject.cxx
+++ ubobj/UBXSec/TPCObject.cxx
@@ -14,7 +14,7 @@ namespace ubana {
   // Setter methoths
   void TPCObject::SetTracks      (std::vector<recob::Track> tracks)    { this->fTracks = tracks;    }
   void TPCObject::SetPFPs        (std::vector<recob::PFParticle> pfps) { this->fPFParticles = pfps; }
-  void TPCObject::SetVertex      (recob::Vertex vertex)                { this->fVertex = vertex;    }       
+  void TPCObject::SetVertex      (recob::Vertex vertex)                { this->fVertex = vertex;    }
   void TPCObject::SetOrigin      (ubana::TPCObjectOrigin origin)       { this->fOrigin = origin;    }
   void TPCObject::SetOriginExtra (ubana::TPCObjectOriginExtra origin)  { this->fOriginExtra = origin;    }
   void TPCObject::SetMultiplicity(int p, int t, int s)                 { this->fPfpMult = p; this->fTrackMult = t; this->fShowerMult = s;}
@@ -38,7 +38,7 @@ namespace ubana {
   const int  TPCObject::GetNTracksCloseToVertex(double tolerance) const {
 
     if (tolerance < 0.) {
-      std::cerr << "[TPCObject] tolerance can't be less than zero" << std::endl; 
+      std::cerr << "[TPCObject] tolerance can't be less than zero" << std::endl;
       throw std::exception();
     }
 
@@ -52,20 +52,60 @@ namespace ubana {
 const auto& vtx = this->fVertex.position();
     // Loop over tracks and calulate multiplicity
     for (auto t : this->fTracks) {
-      
+
    //   TVector3 start = t.Vertex();
      // TVector3 end = t.End();
 
       //if ( (vtx-start).Mag() < tolerance   ||   (vtx-end).Mag() < tolerance ) {
      const auto& start = t.Vertex();
 const auto& end = t.End();
-if ( (vtx-start).R() < tolerance   ||   (vtx-end).R() < tolerance ) {   
+if ( (vtx-start).R() < tolerance   ||   (vtx-end).R() < tolerance ) {
      multiplicity++;
       }
     }
 
     return multiplicity;
   }
+
+
+  std::string TPCObjectOriginToString(TPCObjectOrigin val){
+    switch (val){
+      case kUnknown:
+        return "kUnknown";
+        break;
+      case kBeamNeutrino:
+        return "kBeamNeutrino";
+        break;
+      case kCosmicRay:
+        return "kCosmicRay";
+        break;
+      case kMixed:
+        return "kMixed";
+        break;
+      default:
+        return "???";
+    }
+  }
+
+  std::string TPCObjectOriginExtraToString(TPCObjectOriginExtra val){
+    switch (val){
+      case kNotSet:
+        return "kNotSet";
+        break;
+      case kStoppingMuon:
+        return "kStoppingMuon";
+        break;
+      case kACPT:
+        return "kACPT";
+        break;
+      case kNCPion:
+        return "kNCPion";
+        break;
+      case kNCProton:
+        return "kNCProton";
+        break;
+      default:
+        return "???";
+    }
+  }
 }
-
-
