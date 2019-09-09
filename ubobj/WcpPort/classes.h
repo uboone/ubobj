1b12428d6f838bbcea3f7e7edd05600acf218c5b
Hanyu Wei <weihythu@gmail.com>
2019-09-09 15:01:49 -0500

    WcpPort data products

---
+++ ubobj/WcpPort/classes.h
@@ -0,0 +1,10 @@
+// Build a dictionary
+// brooke.russell@yale.edu
+
+#include "canvas/Persistency/Common/Wrapper.h"
+#include "ubobj/WcpPort/NuSelectionCharge.h"
+#include "ubobj/WcpPort/NuSelectionContainment.h"
+#include "ubobj/WcpPort/NuSelectionMatch.h"
+#include "ubobj/WcpPort/NuSelectionTruth.h"
+
+#include <vector>
